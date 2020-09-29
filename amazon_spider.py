b705f7f97ea301ec27c347bc0bb71735401e56c3
Domenick Botardo <domenickmb@gmail.com>
2020-09-29 15:17:30 +0800

    Set USER_AGENT for AmazonSpider

--- amazon_spider.py
+++ amazon_spider.py
@@ -4,6 +4,10 @@ import scrapy
 class AmazonSpider(scrapy.Spider):
     name = 'amazon'
 
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0',
+        }
+
     def parse(self, response):
         self.item['product_price'] = response.xpath(
             '//span[@id="price_inside_buybox"]/text()').re_first(r'\$(\d+,?.*)\s*')
