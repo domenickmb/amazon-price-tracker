7204959c04d20268b7d96639db3c268e2c824eb2
Domenick Botardo <domenickmb@gmail.com>
2020-10-01 00:08:36 +0800

    Change xpath selector for product price

--- amazon_spider.py
+++ amazon_spider.py
@@ -10,7 +10,7 @@ class AmazonSpider(scrapy.Spider):
 
     def parse(self, response):
         self.item['product_price'] = response.xpath(
-            '//span[@id="price_inside_buybox"]/text()').re_first(r'\$(\d+,?.*)\s*')
+            '//span[@id="priceblock_ourprice"]/text()').re_first(r'\$(\d+,?.*)\s*')
         self.item['product_name'] = response.xpath(
             '//span[@id="productTitle"]/text()').get().strip()
         self.item['product_url'] = response.url
