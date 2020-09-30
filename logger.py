7e5cbe17e9a4d60ed5d64e5828c2bfef05d29f13
Domenick Botardo <domenickmb@gmail.com>
2020-09-30 22:04:34 +0800

    Make logging.StreamHandler use sys.stdout for logging

--- logger.py
+++ logger.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 
 logger = logging.getLogger(__name__)
@@ -7,6 +8,6 @@ my_format = logging.Formatter(
         '%(asctime)s [%(name)s] %(levelname)s: %(message)s',
         datefmt="%Y-%m-%d %H:%M:%S")
 
-my_handler = logging.StreamHandler()
+my_handler = logging.StreamHandler(sys.stdout)
 my_handler.setFormatter(my_format)
 logger.addHandler(my_handler)
